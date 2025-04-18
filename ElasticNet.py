3b17fa6e0c4afbeb4a4a8f03604db973a5468bb0
DiligentBuilder <71938320+DiligentBuilder@users.noreply.github.com>
2025-04-18 00:43:01 -0400

    Update ElasticNet.py
    adding more basic code to begin with will test later

--- ElasticNet.py
+++ ElasticNet.py
@@ -22,3 +22,25 @@ dataframe = read_csv(url, header=None)
 print(dataframe.shape)
 # summarize first few lines
 print(dataframe.head())
+
+from sklearn.linear_model import ElasticNet, ElasticNetCV
+
+# Build the model
+elastic_net_cv = ElasticNetCV(cv=5, random_state=1)
+
+# Train the model
+elastic_net_cv.fit(X_train, y_train)
+
+print(f'Best Alpha: {elastic_net_cv.alpha_}')
+print(f'Best L1 Ratio:{elastic_net_cv.l1_ratio_}')
+
+from sklearn.metrics import mean_squared_error
+
+# Predict values from the test dataset
+elastic_net_pred = elastic_net_cv.predict(X_test)
+
+mse = mean_squared_error(y_test, elastic_net_pred)
+r_squared = elastic_net_cv.score(X_test, y_test)
+
+print(f'Mean Squared Error: {mse}')
+print(f'R-squared value: {r_squared}')
